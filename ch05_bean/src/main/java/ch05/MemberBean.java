c7ba43a16f11b0a7823f30d205565bf31feac8ac
hyeyeon9037 <hyeyeon9037@naver.com>
2024-07-16 18:30:28 +0900

    Create MemberBean.java

---
+++ ch05_bean/src/main/java/ch05/MemberBean.java
@@ -0,0 +1,46 @@
+package ch05;
+
+public class MemberBean {
+	private String id;
+	private String pwd;
+	private String repwd;
+	private String name;
+	private String brithday;
+	private String email;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	public String getRepwd() {
+		return repwd;
+	}
+	public void setRepwd(String repwd) {
+		this.repwd = repwd;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getBrithday() {
+		return brithday;
+	}
+	public void setBrithday(String brithday) {
+		this.brithday = brithday;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
