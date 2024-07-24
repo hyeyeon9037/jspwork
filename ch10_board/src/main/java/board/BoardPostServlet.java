2ee857c433b9988fab149d1fe059474d46533182
hyeyeon9037 <hyeyeon9037@naver.com>
2024-07-24 12:38:30 +0900

    Update BoardPostServlet.java

--- ch10_board/src/main/java/board/BoardPostServlet.java
+++ ch10_board/src/main/java/board/BoardPostServlet.java
@@ -1,3 +1,4 @@
+
 package board;
 
 import jakarta.servlet.ServletException;
@@ -9,10 +10,16 @@ import java.io.IOException;
 
 public class BoardPostServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Board board = new Board();
+		board.setName(request.getParameter("name"));
+		board.setSubject(request.getParameter("subject"));
+		board.setContent(request.getParameter("content"));
+		board.setPass(request.getParameter("pass"));
+		board.setIp(request.getParameter("ip"));
 		
+		boolean result = new BoardDao().insertBoard(board);
+		response.sendRedirect("list.jsp");
 	}
-
 }
