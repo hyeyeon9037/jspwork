5281cce83490956b68f8348055ed5d1de51749b0
hyeyeon9037 <hyeyeon9037@naver.com>
2024-07-26 17:30:51 +0900

    Create AjaxServletReplyList.java

---
+++ ch12_ajax/src/main/java/ajaxReply/AjaxServletReplyList.java
@@ -0,0 +1,29 @@
+package ajaxReply;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import com.google.gson.Gson;
+
+public class AjaxServletReplyList extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		int ref = Integer.parseInt(request.getParameter("bnum"));
+		
+		new ReplyDao().getReplyList(ref);
+		
+		 ArrayList<Reply> alist = new ReplyDao().getReplyList(ref);
+		
+		response.setContentType("application/json; charset=usf-8");
+		new Gson().toJson(alist, response.getWriter());
+		
+	}
+
+}
